8ff2e0efcabc32be00750fea443ce1bc9d3ed441
M7MD5303 <m7md5303@gmail.com>
2023-11-12 01:56:03 +0200

    m

--- src/components/Madina.jsx
+++ src/components/Madina.jsx
@@ -6,7 +6,7 @@ export default function Madina({title,img,para}) {
     return (
         <div className='madina'>
             
-            <div className='mdcont'><div className='cont'> <h1>{title}</h1>
+            <div className='mdcont'><div className='contm'> <h1>{title}</h1>
                 <p>{para}</p></div>
                 <img src={img}/>
             </div>
